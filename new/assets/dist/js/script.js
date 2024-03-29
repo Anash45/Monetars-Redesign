f461522788ab63f3d1fb66b9a1c6f1d8ea20aac1
Syed Anas Bukhari <anas14529@gmail.com>
2024-03-29 10:22:22 +0500

    Sidebar and Modals

--- new/assets/dist/js/script.js
+++ new/assets/dist/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // Initialize Slick carousel
     $('.nav-carousel').slick({
         slidesToShow: 1, // Show 1 slide at a time
@@ -16,4 +16,47 @@ $(document).ready(function(){
     $('.nb-btn').on('click', function () {
         $(this).siblings('.dropdown-menu').toggle();
     })
+
+    function openSidebar() {
+        $('.sidebar').fadeIn();
+    }
+
+    function closeSidebar() {
+        $('.sidebar').fadeOut();
+    }
+
+    $('.sidebar-toggler').on('click', function () {
+        openSidebar();
+    })
+    $('.sidebar').on('click', function (event) {
+        // Check if the clicked element is not inside .side-nav
+        if (!$(event.target).closest('.side-nav').length) {
+            closeSidebar();
+        }
+    });
+    let theme = window.localStorage.getItem('theme');
+    changeTheme(theme);
 });
+
+function changeTheme(targetSwitch) {    
+    let switchBtn = $('.btn-switch');
+    $('body').removeClass('light');
+    $('body').removeClass('dark');
+    $('body').addClass(targetSwitch);
+    window.localStorage.setItem('theme',targetSwitch);
+    if(targetSwitch == 'light'){
+        switchBtn.attr('data-switch','dark');
+    }else{
+        switchBtn.attr('data-switch','light');
+    }
+}
+
+$('.btn-switch').on('click', function (params) {
+    let targetSwitch = $(this).attr('data-switch');
+    changeTheme(targetSwitch);
+});
+
+function openSigninSignup(target) {
+    $('.nav-'+target).tab('show');
+    $('#signinSignup').modal('show');
+}
