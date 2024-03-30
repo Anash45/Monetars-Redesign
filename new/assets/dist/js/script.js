a1dda88d6e6f59d51cdcde9678afba242fe86d9f
Syed Anas Bukhari <anas14529@gmail.com>
2024-03-30 08:47:21 +0500

    First Set Images

--- new/assets/dist/js/script.js
+++ new/assets/dist/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // Initialize Slick carousel
+    // Initialize Slick carousel on Navbar
     $('.nav-carousel').slick({
         slidesToShow: 1, // Show 1 slide at a time
         slidesToScroll: 1, // Scroll 1 slide at a time
@@ -11,6 +11,20 @@ $(document).ready(function () {
         draggable: true, // Enable dragging
         infinite: true, // Infinite looping
         variableWidth: true // Variable width based on content width
+    });
+
+    // Hero carousel
+    $('.hero-carousel').slick({
+        slidesToShow: 1, // Show 1 slide at a time
+        slidesToScroll: 1, // Scroll 1 slide at a time
+        autoplay: true, // Autoplay enabled
+        autoplaySpeed: 2000, // Autoplay speed in milliseconds
+        prevArrow: false, // Hide previous arrow
+        nextArrow: false, // Hide next arrow
+        dots: false, // Hide navigation dots
+        draggable: true, // Enable dragging
+        infinite: true, // Infinite looping
+        variableWidth: false // Variable width based on content width
     });
 
     $('.nb-btn').on('click', function () {
@@ -36,18 +50,36 @@ $(document).ready(function () {
     });
     let theme = window.localStorage.getItem('theme');
     changeTheme(theme);
+
+    $(".testimonials").slick({
+        centerMode: true,
+        centerPadding: '16px',
+        slidesToShow: 1,
+        prevArrow: '<button type="button" class="slick-arrows slick-prev"><i class="fa fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-arrows slick-next"><i class="fa fa-chevron-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '0px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
 
-function changeTheme(targetSwitch) {    
+function changeTheme(targetSwitch) {
     let switchBtn = $('.btn-switch');
     $('body').removeClass('light');
     $('body').removeClass('dark');
     $('body').addClass(targetSwitch);
-    window.localStorage.setItem('theme',targetSwitch);
-    if(targetSwitch == 'light'){
-        switchBtn.attr('data-switch','dark');
-    }else{
-        switchBtn.attr('data-switch','light');
+    window.localStorage.setItem('theme', targetSwitch);
+    if (targetSwitch == 'light') {
+        switchBtn.attr('data-switch', 'dark');
+    } else {
+        switchBtn.attr('data-switch', 'light');
     }
 }
 
@@ -57,6 +89,6 @@ $('.btn-switch').on('click', function (params) {
 });
 
 function openSigninSignup(target) {
-    $('.nav-'+target).tab('show');
+    $('.nav-' + target).tab('show');
     $('#signinSignup').modal('show');
 }
